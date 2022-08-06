70b860832526ca358f4bceb22532cce31e7ad65f
lkiissel <lauri.kiissel@gmail.com>
2022-08-06 13:32:56 +0300

    Started on main page navigation and routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent} from './admin/admin.component';
+import { ClientComponent} from './client/client.component';
+import { BookingComponent} from './booking/booking.component';
+import { ContactComponent} from './contact/contact.component';
+import { AboutComponent} from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminComponent},
+  {path: 'client', component: ClientComponent},
+  {path: 'booking', component: BookingComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
+import { BookingComponent } from './booking/booking.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { UploadComponent } from './admin/upload/upload.component';
+import { ManageComponent } from './admin/manage/manage.component';
+import { CalendarComponent } from './admin/calendar/calendar.component';
+import { ClientsComponent } from './admin/clients/clients.component';
+import { GalleryComponent } from './client/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ClientComponent,
+    BookingComponent,
+    ContactComponent,
+    AboutComponent,
+    UploadComponent,
+    ManageComponent,
+    CalendarComponent,
+    ClientsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
